cd39270a44863749c71a7209fa71f05961666359
bakry56 <abdobakry823@gmail.com>
2026-08-19 15:23:48 +0300

    feat(domain): add TicketStatus and TicketPriority enums

---
+++ src/SupportTicketingPlatform.Domain/Enums/TicketStatus.cs
@@ -0,0 +1,12 @@
+namespace SupportTicketingPlatform.Domain.Enums;
+
+public enum TicketStatus
+{
+    New = 0,
+    Assigned = 1,
+    InProgress = 2,
+    Resolved = 3,
+    Closed = 4,
+    Reopened = 5,
+    Cancelled = 6
+}
