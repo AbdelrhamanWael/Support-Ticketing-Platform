c13863a895e2d86f013c56ad92ae273e2cc0452d
bakry56 <abdobakry823@gmail.com>
2026-08-19 15:23:23 +0300

    feat(domain): add SlaStatus and CommentVisibility enums

---
+++ src/SupportTicketingPlatform.Domain/Enums/CommentVisibility.cs
@@ -0,0 +1,7 @@
+namespace SupportTicketingPlatform.Domain.Enums;
+
+public enum CommentVisibility
+{
+    Public = 0,
+    Internal = 1
+}
---
+++ src/SupportTicketingPlatform.Domain/Enums/SlaStatus.cs
@@ -0,0 +1,8 @@
+namespace SupportTicketingPlatform.Domain.Enums;
+
+public enum SlaStatus
+{
+    OnTrack = 0,
+    AtRisk = 1,
+    Breached = 2
+}
